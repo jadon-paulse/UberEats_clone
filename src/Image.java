dd610ba57a633ca564f3ba9802a77565d8d8abc7
Yusuf69Hendricks <yusuf69hendricks@gmail.com>
2020-08-13 19:23:39 +0200

    Added instance variables to Image ref class

--- src/Image.java
+++ src/Image.java
@@ -1,5 +1,17 @@
 package src;
 
 public class Image {
+
+    private String url;
+    private String description;
+    private String title;
+    private int cost;
+
+    Image(String url, String title, String description, int cost) {
+        this.url = url;
+        this.description = description;
+        this.title = title;
+        this.cost = cost;
+    }
     
 }
