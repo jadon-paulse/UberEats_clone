8ebfa880255a1d36d9a9f4583e25c06b97b474e0
Yusuf69Hendricks <yusuf69hendricks@gmail.com>
2020-08-13 19:29:01 +0200

    added getters and setters

--- src/Image.java
+++ src/Image.java
@@ -14,4 +14,40 @@ public class Image {
         this.cost = cost;
     }
     
+      //getters
+      public String getUrl(){
+        return this.url;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public int getCost(){
+        return this.cost;
+    }
+
+     //setters
+     public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+
+
 }
